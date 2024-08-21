a5f817f80b5788f83e8c8be482f07e83ec2c4b62
Gabriel Dromard <gdromard@gmail.com>
2024-08-21 11:04:16 +0200

    Improve error management

--- requestlog-parser.py
+++ requestlog-parser.py
@@ -1,6 +1,6 @@
 import csv
 import os
-import collections, functools, operator
+import collections
 from colorama import Fore
 from datetime import datetime
 
@@ -10,26 +10,31 @@ from datetime import datetime
 def convert_bytes_to_gb(size_in_bytes):
     gb = size_in_bytes / (1024 ** 3)
     return gb
+
 def display_results(transfert_dict):
     for key, value in transfert_dict.items():
         print(Fore.WHITE + "[" + key + "]", convert_bytes_to_gb(value))
-def process_current_file(file, inbound_dict_list, outbound_dict_list, dialect='piper'):
+
+def process_current_file(file, inbound_aggregation, outbound_aggregation, dialect='piper'):
     with open(file, "r") as csvfile:
         for row in csv.DictReader(csvfile, dialect=dialect, fieldnames=fieldnames):
-            log_timestamp = row['timestamp']
-            http_method = row['request_method']
-            # in bytes
-            current_inbound_length = int(row['request_content_length'])
-            # in bytes
-            current_outbound_length = int(row['response_content_length'])
-            dt = datetime.strptime(log_timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
-            aggregated_log_key = str(dt.month) + "-" + str(dt.year)
-            # now we add the current metric if relevant to the appropriate dict list,
-            # and we ignore HEAD requests as we don't charge our customers for them
-            if((http_method != 'HEAD') and (current_inbound_length > 0)):
-                inbound_dict_list.append({aggregated_log_key : current_inbound_length})
-            if((http_method != 'HEAD') and (current_outbound_length > 0)):
-                outbound_dict_list.append({aggregated_log_key : current_outbound_length})
+            try:
+                log_timestamp = row['timestamp']
+                http_method = row['request_method']
+
+                # Replace '-' with '0' and convert to integer
+                current_inbound_length = int(row['request_content_length']) if row['request_content_length'] != '-' else 0
+                current_outbound_length = int(row['response_content_length']) if row['response_content_length'] != '-' else 0
+
+                dt = datetime.strptime(log_timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+                aggregated_log_key = f"{dt.month}-{dt.year}"
+
+                if http_method != 'HEAD':
+                    if current_inbound_length > 0: inbound_aggregation[aggregated_log_key] += current_inbound_length
+                    if current_outbound_length > 0: outbound_aggregation[aggregated_log_key] += current_outbound_length
+            except ValueError:
+                print(Fore.RED + f"Invalid data format in file {file}")
+                continue
     return
 
 ##########################
@@ -40,28 +45,20 @@ logs_directory = "logs_to_process"
 csv.register_dialect('piper', delimiter='|', quoting=csv.QUOTE_NONE)
 csv.register_dialect('comma', delimiter=',', quoting=csv.QUOTE_NONE)
 
-fieldnames = ['timestamp' ,'trace_id','remote_address','username ','request_method','request_url','return_status','request_content_length','response_content_length','request_duration','request_user_agent']
+fieldnames = ['timestamp', 'trace_id', 'remote_address', 'username ', 'request_method', 'request_url', 'return_status', 'request_content_length', 'response_content_length', 'request_duration', 'request_user_agent']
 
-inbound_dict_list = []
-outbound_dict_list = []
+inbound_aggregation = collections.defaultdict(int)
+outbound_aggregation = collections.defaultdict(int)
 
 # process all files in directory
-for root,dirs,files in os.walk(logs_directory):
+for root, dirs, files in os.walk(logs_directory):
     for file in files:
-       
-       if file.endswith(".log"):
-           file_path = os.path.join(logs_directory, file)
-           print(f"processing {file_path}")
-           # change separator if needed
-           process_current_file(file_path, inbound_dict_list, outbound_dict_list, 'piper')
+        if file.endswith(".log"):
+            file_path = os.path.join(logs_directory, file)
+            print(Fore.YELLOW + f"Processing {file_path}...")
+            process_current_file(file_path, inbound_aggregation, outbound_aggregation, 'piper')
 
-    inbound_aggregation = dict(functools.reduce(operator.add,
-            map(collections.Counter, inbound_dict_list)))
-    outbound_aggregation = dict(functools.reduce(operator.add,
-            map(collections.Counter, outbound_dict_list)))
-    
-    print(Fore.GREEN + "Total Inboud results per month")
-    display_results(inbound_aggregation)
-    print(Fore.GREEN + "Total Outboud results per month")
-    display_results(outbound_aggregation)  
-    
+print(Fore.GREEN + "Total Inbound results per month")
+display_results(inbound_aggregation)
+print(Fore.GREEN + "Total Outbound results per month")
+display_results(outbound_aggregation)
